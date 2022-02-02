6d8770dc4f0888ad681c3c17f5d9b260f32324ca
Holley Voegtle <hvoegtle@gmail.com>
2022-02-02 10:52:50 -0800

    adds more files

--- wdio.conf.js
+++ wdio.conf.js
@@ -54,15 +54,29 @@ exports.config = {
         // maxInstances can get overwritten per capability. So if you have an in-house Selenium
         // grid with only 5 firefox instances available you can make sure that not more than
         // 5 instances get started at a time.
-        maxInstances: 5,
+            maxInstances: 5,
         //
-        browserName: 'chrome',
-        acceptInsecureCerts: true
+            browserName: 'chrome',
+            acceptInsecureCerts: true
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
         // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
         // excludeDriverLogs: ['bugreport', 'server'],
-    }],
+        },
+        {
+            maxInstances: 5,
+            browserName: 'MicrosoftEdge',
+            acceptInsecureCerts: true
+        },
+        {
+            maxInstances: 5,
+            browserName: 'firefox',
+            acceptInsecureCerts: true
+        }
+
+
+
+],
     //
     // ===================
     // Test Configurations
@@ -110,7 +124,7 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['chromedriver'],
+    services: [],
     
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
